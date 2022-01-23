7ae59a0b26eaab579ace55d2e3adf675c31c8488
spoonya <nomad8t@gmail.com>
2022-01-23 20:35:42 +0300

    refactor

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,13 @@
 import { data } from "./data.js";
 
+const questions = {
+  sites: "sites",
+  packages: "packages",
+  services: "services",
+  engines: "engines",
+  design: "design",
+};
+
 class Quiz {
   constructor(selector, data) {
     this._quiz = document.querySelector(selector);
@@ -61,6 +69,50 @@ class Quiz {
     this._answers.innerHTML = "";
   }
 
+  _displayPrevButton() {
+    if (this._questionNumber) {
+      this._prevBtn.classList.add("active");
+    } else {
+      this._prevBtn.classList.remove("active");
+    }
+  }
+
+  _hideControls() {
+    this._controls.classList.add("hidden");
+  }
+
+  _initPrevButton() {
+    this._prevBtn.addEventListener("click", () => {
+      let input;
+
+      if (this._answers.querySelector("input:checked")) {
+        input = this._answers.querySelector("input:checked");
+      } else {
+        input = this._answers.querySelector("input");
+      }
+
+      this._questionNumber -= 1;
+      this._displayPrevButton();
+
+      this._createQuestion(this._prevQuestion, input);
+    });
+  }
+
+  _initNextButton() {
+    this._nextBtn.addEventListener("click", () => {
+      const checkedInput = this._answers.querySelector("input:checked");
+
+      if (!checkedInput) return;
+
+      this._nextQuestion = checkedInput.dataset.quizNextQuestion;
+
+      this._questionNumber += 1;
+      this._displayPrevButton();
+
+      this._createQuestion(this._nextQuestion, checkedInput);
+    });
+  }
+
   _appendResult(label, result) {
     this._resultContainer.insertAdjacentHTML(
       "beforeend",
@@ -116,6 +168,33 @@ class Quiz {
     this._createResultCost();
   }
 
+  _createAnswers(
+    obj,
+    {
+      nextQuestion = "",
+      prevQuestion = this._prevQuestion,
+      devBranch = false,
+      packageBranch = false,
+    }
+  ) {
+    for (const [key, value] of Object.entries(obj)) {
+      this._answers.insertAdjacentHTML(
+        "beforeend",
+        this._createInput({
+          label: value.input ? value.input.label : value.label,
+          type: value.input ? value.input.type : value.type,
+          name: value.input ? value.input.name : value.name,
+          value: value.input ? value.input.value : value.value,
+          checked: value.input ? value.input.checked : value.checked,
+          devBranch: devBranch ? key : this._devBranch,
+          packageBranch: packageBranch ? key : this._packageBranch,
+          nextQuestion: nextQuestion,
+          prevQuestion: prevQuestion,
+        })
+      );
+    }
+  }
+
   _createQuestion(question, checkedInput) {
     if (checkedInput) {
       if (checkedInput.dataset.quizDevBranch) {
@@ -132,23 +211,13 @@ class Quiz {
     this._clearAnswers();
 
     switch (question) {
-      case "sites":
+      case questions.sites:
         this._question.innerText = data.initQuestionLabel;
 
-        for (const [key, value] of Object.entries(this._data.sites)) {
-          this._answers.insertAdjacentHTML(
-            "beforeend",
-            this._createInput({
-              label: value.input.label,
-              type: value.input.type,
-              name: value.input.name,
-              value: value.input.value,
-              checked: value.input.checked,
-              devBranch: key,
-              nextQuestion: "packages",
-            })
-          );
-        }
+        this._createAnswers(this._data.sites, {
+          nextQuestion: questions.packages,
+          devBranch: true,
+        });
 
         this._answers.querySelectorAll("input").forEach((radio) => {
           radio.addEventListener("change", () => {
@@ -166,27 +235,15 @@ class Quiz {
 
         break;
 
-      case "packages":
+      case questions.packages:
         this._question.innerText = this._data.packages.question;
 
-        this._prevQuestion = "sites";
+        this._prevQuestion = questions.sites;
 
-        for (const [key, value] of Object.entries(this._data.packages.types)) {
-          this._answers.insertAdjacentHTML(
-            "beforeend",
-            this._createInput({
-              label: value.input.label,
-              type: value.input.type,
-              name: value.input.name,
-              value: value.input.value,
-              checked: value.input.checked,
-              devBranch: this._devBranch,
-              packageBranch: key,
-              nextQuestion: this._data.packages.nextQuestion,
-              prevQuestion: this._prevQuestion,
-            })
-          );
-        }
+        this._createAnswers(this._data.packages.types, {
+          nextQuestion: this._data.packages.nextQuestion,
+          packageBranch: true,
+        });
 
         this._answers.querySelectorAll("input").forEach((radio) => {
           radio.addEventListener("change", () => {
@@ -206,31 +263,19 @@ class Quiz {
 
         break;
 
-      case "services":
+      case questions.services:
         this._question.innerText =
           this._data.sites[this._devBranch].packages.question;
 
-        this._prevQuestion = "packages";
+        this._prevQuestion = questions.packages;
 
-        for (const [_, value] of Object.entries(
-          this._data.sites[this._devBranch].packages.types[this._packageBranch]
-        )) {
-          this._answers.insertAdjacentHTML(
-            "beforeend",
-            this._createInput({
-              label: value.label,
-              type: value.type,
-              name: value.name,
-              value: value.value,
-              checked: value.checked,
-              devBranch: this._devBranch,
-              packageBranch: this._packageBranch,
-              nextQuestion:
-                this._data.sites[this._devBranch].packages.nextQuestion,
-              prevQuestion: this._prevQuestion,
-            })
-          );
-        }
+        this._createAnswers(
+          this._data.sites[this._devBranch].packages.types[this._packageBranch],
+          {
+            nextQuestion:
+              this._data.sites[this._devBranch].packages.nextQuestion,
+          }
+        );
 
         this._answers.querySelectorAll("input").forEach((checkbox) => {
           checkbox.addEventListener("change", () => {
@@ -254,31 +299,15 @@ class Quiz {
 
         break;
 
-      case "engines":
+      case questions.engines:
         this._question.innerText =
           this._data.sites[this._devBranch].engines.question;
 
-        this._prevQuestion = "services";
+        this._prevQuestion = questions.services;
 
-        for (const [_, value] of Object.entries(
-          this._data.sites[this._devBranch].engines.types
-        )) {
-          this._answers.insertAdjacentHTML(
-            "beforeend",
-            this._createInput({
-              label: value.label,
-              type: value.type,
-              name: value.name,
-              value: value.value,
-              checked: value.checked,
-              devBranch: this._devBranch,
-              packageBranch: this._packageBranch,
-              nextQuestion:
-                this._data.sites[this._devBranch].engines.nextQuestion,
-              prevQuestion: this._prevQuestion,
-            })
-          );
-        }
+        this._createAnswers(this._data.sites[this._devBranch].engines.types, {
+          nextQuestion: this._data.sites[this._devBranch].engines.nextQuestion,
+        });
 
         this._answers.querySelectorAll("input").forEach((radio) => {
           radio.addEventListener("change", () => {
@@ -298,31 +327,18 @@ class Quiz {
 
         break;
 
-      case "design":
+      case questions.design:
         this._question.innerText = this._data.design.question;
 
         if (this._devBranch !== "landing") {
-          this._prevQuestion = "engines";
+          this._prevQuestion = questions.engines;
         } else {
-          this._prevQuestion = "services";
+          this._prevQuestion = questions.services;
         }
 
-        for (const [_, value] of Object.entries(this._data.design.types)) {
-          this._answers.insertAdjacentHTML(
-            "beforeend",
-            this._createInput({
-              label: value.label,
-              type: value.type,
-              name: value.name,
-              value: value.value,
-              checked: value.checked,
-              devBranch: this._devBranch,
-              packageBranch: this._packageBranch,
-              nextQuestion: this._data.design.nextQuestion,
-              prevQuestion: this._prevQuestion,
-            })
-          );
-        }
+        this._createAnswers(this._data.design.types, {
+          nextQuestion: this._data.design.nextQuestion,
+        });
 
         this._answers.querySelectorAll("input").forEach((radio) => {
           radio.addEventListener("change", () => {
@@ -341,62 +357,17 @@ class Quiz {
         break;
 
       default:
-        this._hideControls();
         this._question.innerText = "Результат";
+        this._hideControls();
         this._createResult();
         break;
     }
   }
 
-  _displayPrevButton() {
-    if (this._questionNumber) {
-      this._prevBtn.classList.add("active");
-    } else {
-      this._prevBtn.classList.remove("active");
-    }
-  }
-
-  _hideControls() {
-    this._controls.classList.add("hidden");
-  }
-
-  _initPrevButton() {
-    this._prevBtn.addEventListener("click", () => {
-      let input;
-
-      if (this._answers.querySelector("input:checked")) {
-        input = this._answers.querySelector("input:checked");
-      } else {
-        input = this._answers.querySelector("input");
-      }
-
-      this._questionNumber -= 1;
-      this._displayPrevButton();
-
-      this._createQuestion(this._prevQuestion, input);
-    });
-  }
-
-  _initNextButton() {
-    this._nextBtn.addEventListener("click", () => {
-      const checkedInput = this._answers.querySelector("input:checked");
-
-      if (!checkedInput) return;
-
-      this._nextQuestion = checkedInput.dataset.quizNextQuestion;
-
-      this._questionNumber += 1;
-      this._displayPrevButton();
-
-      this._createQuestion(this._nextQuestion, checkedInput);
-    });
-  }
-
   init() {
+    this._createQuestion(data.initQuestion);
     this._initNextButton();
     this._initPrevButton();
-
-    this._createQuestion(data.initQuestion);
   }
 }
 
